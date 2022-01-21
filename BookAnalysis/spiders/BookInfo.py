acfd9add2f3a68f1094b3fad41d15dc712bedf5d
Ruan-XinWei <7767928+ruanxinwei@user.noreply.gitee.com>
2022-01-21 16:48:54 +0800

    feat: 添加 书籍详情页爬虫(待完善) 和 requirements.txt

---
+++ BookAnalysis/spiders/BookInfo.py
@@ -0,0 +1,39 @@
+import requests
+from lxml import etree
+
+
+class BookInfo:
+    def __init__(self):
+        self._url = 'https://book.douban.com/subject/35534519/'
+        self._method = 'GET'
+        self._headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+        }
+
+    def crawl(self):
+        resp = requests.request(method=self._method, url=self._url, headers=self._headers)
+        content = resp.content.decode(resp.encoding)
+        return content
+
+    def parse(self, content):
+        # 这里注意要按照 列表顺序来
+        _ans = []
+        root = etree.HTML(content)
+        info = root.xpath('//*[@id="info"]')[0]
+        spans = info.xpath('./span[@class="pl"]')
+        for span in spans:
+            print(span.xpath('string(.)'), span.tail.strip())
+            if span.xpath('string(.)') == '出版社:':
+                _ans.append(span.tail.strip())
+        print(_ans)
+
+    def run(self):
+        content = self.crawl()
+        self.parse(content)
+
+        # //*[@id="info"]
+
+
+if __name__ == '__main__':
+    bookInfo = BookInfo()
+    bookInfo.run()
